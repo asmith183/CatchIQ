d75a66e8ee8c97d040d93d7b67853ccab035a852
asmith183 <asmith183@huskers.unl.edu>
2026-08-08 23:30:49 -0500

    Built AppLayout and nav bar

---
+++ frontend/src/pages/History.tsx
@@ -0,0 +1,7 @@
+function History() {
+    return (
+        <h1 className="text-3xl font-bold">History</h1>
+    );
+}
+
+export default History; 
---
+++ frontend/src/pages/Insights.tsx
@@ -0,0 +1,7 @@
+function Insights() {
+    return (
+        <h1 className="text-3xl font-bold">Insights</h1>
+    );
+}
+
+export default Insights;
